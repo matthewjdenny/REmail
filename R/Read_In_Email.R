1d60d08b9fb11e90e674fd48db8675dead42b74e
Matt Denny <matthewjdenny@gmail.com>
2015-05-24 16:51:05 -0400

    documented function

--- R/Read_In_Email.R
+++ R/Read_In_Email.R
@@ -1,6 +1,11 @@
+#' A function to extract To, From, Date, Subject, and Message fields from a plain text email.
+#' 
+#' @param email_file The full path to the email file.
+#' @return An object of class Email that contains the relevant information
+#' @examples
+#' Read_In_Email("/Users/matthewjdenny/Dropbox/PINLab/Raw_Emails/2")
 Read_In_Email <- function(email_file){
   email1 = new("Email",filename=email_file)
   email1 = getElement(email1)
-  #email1@subject
   return(email1)
 }
