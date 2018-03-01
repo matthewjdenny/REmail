2f54b27eb6b64f2cc6d60392e31c465b68b7aa5d
Matt Denny <matthewjdenny@gmail.com>
2018-03-01 17:05:40 -0500

    version bump

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("REmail: Basic E-Mail Parsing with Python
+                        Version 0.2.0 created on 2018-03-01.
+                        copyright (c) 2018, Matthew J. Denny, Penn State
+                        Development website: github.com/matthewjdenny/REmail
+                        Contact: matthewjdenny@gmail.com")
+}
