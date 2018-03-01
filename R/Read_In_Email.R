a8bde865ea700445a23e19b33257d2a7de1043ed
Matt Denny <matthewjdenny@gmail.com>
2018-03-01 17:07:06 -0500

    better example

--- R/Read_In_Email.R
+++ R/Read_In_Email.R
@@ -3,9 +3,11 @@
 #' @param email_file The full path to the email file.
 #' @return An object of class Email that contains the following members: to, from, CC, subject, date, message, message_tokenized, subject_tokenized, num_recipients, all_tokens, num_tokens
 #' @examples
-#' MyEmail <- Read_In_Email("/Users/matthewjdenny/Dropbox/PINLab/Raw_Emails/2")
+#' files <- get_file_paths()
+#' MyEmail <- Read_In_Email(files[1])
+#' @export
 Read_In_Email <- function(email_file){
-  email1 = new("Email",filename=email_file)
+  email1 = new("Email",filename = email_file)
   email1 = getElement(email1)
   email1 = Clean_Email_Input(email1)
   return(email1)
